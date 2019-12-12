097c28e21946876a7932db28ec2e992844ff30a0
MasatoMakino <unagiinu.g@gmail.com>
2019-12-12 16:59:32 +0900

    update : devDependencies

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,16 +1,16 @@
 const { dest, parallel, series, src, watch } = require("gulp");
 
 const doc = require("gulptask-tsdoc").get();
-const server = require("gulptask-dev-server")("./docs/demo");
+const server = require("gulptask-dev-server").get("./docs/demo");
 
 const copyGlob = "./demoSrc/**/*.{html,png,jpg,jpeg}";
 const copy = () => {
   return src(copyGlob, { base: "./demoSrc/" }).pipe(dest("./docs/demo"));
 };
 
-const { bundleDevelopment, watchBundle } = require("gulptask-webpack")(
-  "./webpack.config.js"
-);
+const { bundleDevelopment, watchBundle } = require("gulptask-webpack").get({
+  configPath: "./webpack.config.js"
+});
 const { tsc, tscClean, watchTsc } = require("gulptask-tsc").get();
 
 const watchTasks = cb => {
